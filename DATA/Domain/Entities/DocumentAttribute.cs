81ce6e20a66227c772dfdca38ae055c72f491e10
john <chukwuchidieberejohn@gmail.com>
2023-01-23 22:50:11 +0100

    initial commit

--- DATA/Domain/Entities/DocumentAttribute.cs
+++ DATA/Domain/Entities/DocumentAttribute.cs
@@ -1,9 +1,15 @@
 namespace DATA.Domain.Entities;
 
 [AttributeUsage(AttributeTargets.All)]
+[Document(Description = "Models", Input = "Content models", Output = "none")]
 public class DocumentAttribute : Attribute
 {
+    [Document(Description = "Stores description", Input = "none", Output = "none\n")]
     public string Description { get; set; }
+
+    [Document(Description = "Stores input", Input = "none", Output = "none\n")]
     public string Input { get; set; }
+
+    [Document(Description = "Returns output", Input = "none", Output = "none\n")]
     public string Output { get; set; }
 }
